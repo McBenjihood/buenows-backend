b93c519aa0fc4c89365348ea15b87a2da93abecc
Benji <benjaminmikagerresheim@gmail.com>
2026-01-22 15:13:56 +0100

    Initial commit

---
+++ src/main/java/com/buenws/buenws_backend/BuenwsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.buenws.buenws_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuenwsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuenwsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buenws/buenws_backend/BuenwsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buenws.buenws_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuenwsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
