71778d5d0d6bf6d8017a5ce0a2d250cdef78d42d
Liam Bruno <liambruno007@gmail.com>
2026-04-27 18:39:58 +0200

    Add default admin initializer

--- src/main/java/com/buenws/buenws_backend/API/Configuration/DataInitializer.java
+++ src/main/java/com/buenws/buenws_backend/API/Configuration/DataInitializer.java
@@ -1,10 +1,14 @@
 package com.buenws.buenws_backend.API.Configuration;
 
+import com.buenws.buenws_backend.API.Entity.RefreshTokenEntity;
 import com.buenws.buenws_backend.API.Entity.UserEntity;
-import com.buenws.buenws_backend.API.Repository.Repositories.UserRepository;import org.springframework.boot.CommandLineRunner;
+import com.buenws.buenws_backend.API.Repository.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
 import java.util.List;
 
 @Component
@@ -12,6 +16,12 @@ public class DataInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+    @Value("${admin.email}")
+    private String adminEmail;
+
+    @Value("${admin.password}")
+    private String adminPassword;
 
     public DataInitializer(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
@@ -23,17 +33,25 @@ public class DataInitializer implements CommandLineRunner {
         boolean adminExists = userRepository.findAll()
                 .stream()
                 .anyMatch(user -> user.getAuthorities() != null &&
-                        user.getAuthorities().contains("ADMIN"));
+                        user.getAuthorities().contains("ROLE_ADMIN"));
 
         if (!adminExists) {
             UserEntity admin = new UserEntity();
-            admin.setEmail("admin@buenows.com");
-            admin.setPassword(passwordEncoder.encode("admin123"));
-            admin.setAuthorities(List.of("ADMIN"));
+            admin.setEmail(adminEmail);
+            admin.setPassword(passwordEncoder.encode(adminPassword));
+            admin.setAuthorities(List.of("ROLE_ADMIN"));
+
+            RefreshTokenEntity refreshToken = new RefreshTokenEntity();
+            refreshToken.setToken("");
+            refreshToken.setEdited_at(Instant.now());
+            refreshToken.setExpires_at(Instant.now());
+            refreshToken.setUserEntity(admin);
+
+            admin.setRefreshTokenEntity(refreshToken);
 
             userRepository.save(admin);
 
-            System.out.println("Admin created: admin@buenows.com");
+            System.out.println("Admin created: " + adminEmail);
         }
     }
 }
