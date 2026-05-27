5f3c0049ca63d3e24e166a6d55a831f65452bdab
Benjamin Gerresheim <benjaminmikagerresheim@gmail.com>
2026-05-27 17:56:07 +0200

    - Trying to get deployment to AWS to work

---
+++ src/main/java/com/buenws/buenws_backend/Util/DbConfigDebug.java
@@ -0,0 +1,23 @@
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+class DbConfigDebug implements CommandLineRunner {
+
+    @Value("${spring.datasource.url:NOT_SET}")
+    private String datasourceUrl;
+
+    @Value("${username:NOT_SET}")
+    private String username;
+
+    @Value("${host:NOT_SET}")
+    private String host;
+
+    @Override
+    public void run(String... args) {
+        System.out.println("DEBUG_DB_HOST=" + host);
+        System.out.println("DEBUG_DB_USERNAME_SET=" + !"NOT_SET".equals(username));
+        System.out.println("DEBUG_DATASOURCE_URL=" + datasourceUrl);
+    }
+}
